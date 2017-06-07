da86ff412bf74f2a1103c944ddd5c0bde9d1591c
Alexander1994 <little_al14@hotmail.com>
2017-06-07 16:06:38 -0300

    basic outline for temperature class, small updates to temperature unit tests to match class

--- app/src/main/java/com/teamplantpower/team_plant_power/Temperature.java
+++ app/src/main/java/com/teamplantpower/team_plant_power/Temperature.java
@@ -1,8 +1,37 @@
 package com.teamplantpower.team_plant_power;
 
-/**
- * Created by Brandon on 2017-05-31.
- */
 
 public class Temperature {
+    double minRange, maxRange, celciusValue;
+
+    Temperature() {
+        minRange = Double.NaN;
+        maxRange = Double.NaN;
+        celciusValue = Double.NaN;
+    }
+
+    public boolean setRange(double min, double max) {
+        if (min <= max) {
+            minRange = min;
+            maxRange = max;
+            return true;
+        }
+        return false;
+    }
+    public boolean setValue(double val){
+        if (val <= maxRange &&
+                val >= minRange &&
+                !Double.isNaN(minRange) &&
+                !Double.isNaN(maxRange)) {
+            celciusValue = val;
+            return true;
+        }
+        return false;
+    }
+    public double getFarenheitValue() {
+        return celciusValue * 1.8 + 32;
+    }
+    public double getCelciusValue() {
+        return celciusValue;
+    }
 }
--- app/src/test/java/com/teamplantpower/team_plant_power/TemperatureUnitTest.java
+++ app/src/test/java/com/teamplantpower/team_plant_power/TemperatureUnitTest.java
@@ -8,23 +8,23 @@ public class TemperatureUnitTest {
 
     @Test
     public void rangeIsValid() throws Exception {
-        Temperature t new Temperature();
+        Temperature t = new Temperature();
         assertEquals(t.setRange(20, 10), false);
     }
     @Test
     public void rangeIsNotSetBeforeValue() throws Exception {
-        Temperature t new Temperature();
+        Temperature t = new Temperature();
         assertEquals(t.setValue(20), false);
     }
     @Test
     public void valueIsBeyondMax() throws Exception {
-        Temperature t new Temperature();
+        Temperature t = new Temperature();
         t.setRange(10,30);
         assertEquals(t.setValue(40), false);
     }
     @Test
     public void valueIsLessThanMin() throws Exception {
-        Temperature t new Temperature();
+        Temperature t = new Temperature();
         t.setRange(10,30);
         assertEquals(t.setValue(5), false);
     }
@@ -32,8 +32,7 @@ public class TemperatureUnitTest {
     public void farenheitConversionOfTemperature() throws Exception {
         Temperature t = new Temperature();
         t.setRange(10, 30);
-        t.setCelciusValue(20);
-        assertEquals(t.getFarenheitValue(), 68);
+        t.setValue(20);
+        assertEquals(t.getFarenheitValue() == 68, true);
     }
-
 }
