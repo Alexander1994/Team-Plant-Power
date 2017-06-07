ef8670d99e6bf1fd43e24bd941c935a225d6fe0e
ClearJade <clearjade96@hotmail.com>
2017-06-07 15:48:43 -0300

    Initial UI for temperature, light, humidity.

---
+++ app/src/main/java/com/teamplantpower/team_plant_power/LightDisplay.java
@@ -0,0 +1,13 @@
+package com.teamplantpower.team_plant_power;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LightDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light_display);
+    }
+}
