01cad4dec45e4d81d6fbc6d78c4274eb22e75e52
ClearJade <clearjade96@hotmail.com>
2017-06-08 13:39:39 -0300

    Implemented basic navigation for home screen. Fixed build issue.

--- app/src/main/java/com/teamplantpower/team_plant_power/MainActivity.java
+++ app/src/main/java/com/teamplantpower/team_plant_power/MainActivity.java
@@ -66,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
