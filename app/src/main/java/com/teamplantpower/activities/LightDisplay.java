d72e32240348e15079f6673fe559062d3bef2d9b
MishalN <alnajdi.mishal@gmail.com>
2017-07-05 16:13:55 -0300

    Retrieve light value from firebase database by (Mishal & Najla)

--- app/src/main/java/com/teamplantpower/activities/LightDisplay.java
+++ app/src/main/java/com/teamplantpower/activities/LightDisplay.java
@@ -2,17 +2,25 @@ package com.teamplantpower.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.teamplantpower.team_plant_power.Database;
 import com.teamplantpower.team_plant_power.Light;
 import com.teamplantpower.team_plant_power.R;
 import com.teamplantpower.team_plant_power.Range;
 
+
 public class LightDisplay extends AppCompatActivity {
+    private static final String TAG = "LightDisplay";
     //UI Elements
     TextView lightExposureValue;
     Button refreshLightExposure;
@@ -24,6 +32,11 @@ public class LightDisplay extends AppCompatActivity {
     Database data;
     Light light_exposure;
     Range lightRange;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("currentLight");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +54,26 @@ public class LightDisplay extends AppCompatActivity {
         setMin_MaxLight = (Button) findViewById(R.id.setLight);
         message = (TextView) findViewById(R.id.message);
         //Display Current Light Exposure Values on UI
+// Read from the database
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                lightExposureValue.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
         lightExposureValue.setText(Double.toString(light_exposure.getPercentLight()));
         setMinLight.setText(Double.toString(lightRange.getMinRange()));
         setMaxLight.setText(Double.toString(lightRange.getMaxRange()));
